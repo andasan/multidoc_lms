bf28872fa1fc43686d690eae871320408ac2a05a
andasan <andasan@gmail.com>
2024-11-08 00:26:07 +0000

    Build: (ebc7fdf) feat: improvise sidepanel alignments

--- assets/company.constants-B4NxR415.js
+++
@@ -1,1 +0,0 @@
-const t="Multidoc LMS",x="#xxx-xxx Cloverdale St",o="City, State, Zipcode",e="(xxx) xxx-xxxx",l="info@multidoclms.com",s=[{text:t,bold:!0},{text:x,bold:!1},{text:o,bold:!1},{text:e,bold:!1},{text:l,bold:!1}];export{t as C,s as a};
---
+++ assets/logo-C5CzYpGe.js
@@ -0,0 +1,1 @@
+const o="/assets/logo-MjXVjvvC.png";export{o as f};
